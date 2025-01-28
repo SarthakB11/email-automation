3949e68e3058636f15725f3270c5e91c56d59171
SarthakB11 <100398847+SarthakB11@users.noreply.github.com>
2025-01-28 16:50:22 +0530

    adding html formatting

--- main.py
+++ main.py
@@ -1,39 +1,35 @@
+import os
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
 SMTP_SERVER = 'smtp.example.com'
 SMTP_PORT = 587
-SMTP_USERNAME = 'your_email@example.com'
-SMTP_PASSWORD = 'your_password'
+SMTP_USERNAME = os.environ.get('USERNAME')
+SMTP_PASSWORD = os.environ.get('PASSWORD')
 
 email_addresses = ['recipient1@example.com', 'recipient2@example.com', 'recipient3@example.com']
 subjects = ['Subject 1', 'Subject 2', 'Subject 3']
 names = ['Name 1', 'Name 2', 'Name 3']
 
 email_body_template = """
-Dear {name},
-
-This is a personalized email for you.
-
-Best regards,
-Your Name
+Here is your email body.
 """
 
 def send_email(to_email, subject, body):
-
     msg = MIMEMultipart()
     msg['From'] = SMTP_USERNAME
     msg['To'] = to_email
     msg['Subject'] = subject
 
-    msg.attach(MIMEText(body, 'plain'))
+    msg.attach(MIMEText(body, 'html'))
 
     try:
-    
+
         server = smtplib.SMTP(SMTP_SERVER, SMTP_PORT)
         server.starttls()
         server.login(SMTP_USERNAME, SMTP_PASSWORD)
+
 
         server.sendmail(SMTP_USERNAME, to_email, msg.as_string())
         print(f"Email sent to {to_email}")
@@ -42,12 +38,18 @@ def send_email(to_email, subject, body):
         print(f"Failed to send email to {to_email}: {e}")
 
     finally:
+
         server.quit()
 
 def main():
+    resume_link = os.environ.get('RESUME_LINK')
+    linkedin_link = os.environ.get('LINKEDIN_LINK')
+    github_link = os.environ.get('GITHUB_LINK')
+
     for email, subject, name in zip(email_addresses, subjects, names):
-    
-        body = email_body_template.format(name=name)
+
+        body = email_body_template.format(name=name, RESUME_LINK=resume_link, LINKEDIN_LINK=linkedin_link, GITHUB_LINK=github_link)
+
 
         send_email(email, subject, body)
 
